08ce47a87671a132a8144440d94b7ecdc2a7510d
wangqian <wangqian@antiy.cn>
2021-03-25 18:21:38 +0800

    Spring-Boot实现接口异步访问

--- src/main/java/com/antony/GoodcodedemoApplication.java
+++ src/main/java/com/antony/GoodcodedemoApplication.java
@@ -2,10 +2,13 @@ package com.antony;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+//开启CGLIB代理
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class GoodcodedemoApplication {
 
     public static void main(String[] args) {
